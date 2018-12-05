d65f9e5f516d1ae8cde024d7e19ec83311352b54
Loginov Ilya <dantes2104@gmail.com>
2018-12-06 00:36:06 +0400

    Add installing mysql in reference

--- install_reference_Ubuntu.sh
+++ install_reference_Ubuntu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install mysql
 cd ~/
 npm i http https fs path request mysql querystring express body-parser express-fileupload bcrypt async cookie-parser
