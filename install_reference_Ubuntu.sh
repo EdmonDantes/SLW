2a6a6a539236d696e46cdd463adf45b97a28a608
Loginov Ilya <dantes2104@gmail.com>
2019-01-15 01:58:07 +0400

    Adding graphicsmagick for install script

--- install_reference_Ubuntu.sh
+++ install_reference_Ubuntu.sh
@@ -2,5 +2,6 @@
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install mysql-server
+sudo apt-get install graphicsmagick
 cd ~/
 npm i http https fs path request mysql querystring express body-parser express-fileupload bcrypt async cookie-parser gm node-rsa swagger-ui-dist
